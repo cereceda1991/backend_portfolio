ac69ed0a50e1d5f499542729e335843566b8b8dd
cereceda1991 <cereceda1991@gmail.com>
2023-11-14 11:57:46 -0500

    feat: add translation in errors messages

--- src/config/expressValidator-config.js
+++ src/config/expressValidator-config.js
@@ -69,54 +69,53 @@ export const validationConfig = {
   },
   isURL: {
     validator: (fieldName) => body(fieldName).isURL(),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser una URL válida.`,
+    errorMessage: () => `The field must be a valid URL.`,
   },
   isNumeric: {
     validator: (fieldName) => body(fieldName).isNumeric(),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser un número`,
+    errorMessage: () => `The field must be a number.`,
   },
   isInt: {
     validator: (fieldName) => body(fieldName).isInt(),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser un número entero`,
+    errorMessage: () => `The field must be an integer.`,
   },
   isFloat: {
     validator: (fieldName) => body(fieldName).isFloat(),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser un número decimal`,
+    errorMessage: () => `The field must be a decimal number.`,
   },
   isBoolean: {
     validator: (fieldName) => body(fieldName).isBoolean(),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser un valor booleano (true o false)`,
+    errorMessage: () => `The field must be a boolean value (true or false).`,
   },
   isDate: {
     validator: (fieldName) => body(fieldName).isISO8601(),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser una fecha válida`,
+    errorMessage: () => `The field must be a valid date.`,
   },
   isAfter: {
     validator: (fieldName, date) => body(fieldName).isAfter(date),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser una fecha posterior a la fecha proporcionada`,
+    errorMessage: () => `The field must be a date later than the provided date.`,
   },
   isBefore: {
     validator: (fieldName, date) => body(fieldName).isBefore(date),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser una fecha anterior a la fecha proporcionada`,
+    errorMessage: () => `The field must be a date earlier than the provided date.`,
   },
   isMongoId: {
     validator: (fieldName) => body(fieldName).isMongoId(),
-    errorMessage: (fieldName) => `El campo ${fieldName} debe ser un ObjectId válido de MongoDB.`,
+    errorMessage: () => `The field must be a valid MongoDB ObjectId.`,
   },
 
   // Agrega más reglas de validación nativas aquí...
 
   //Reglas Personalizadas
-
   notEmptyFile: {
     validator: (fieldName) =>
       body(fieldName).custom((value, { req }) => {
         if (!req.file) {
-          throw new Error(`El campo ${fieldName} es obligatorio.`);
+          throw new Error(`The ${fieldName} field is required.`);
         }
         return true;
       }),
-    errorMessage: (fieldName) => `El campo ${fieldName} es obligatorio.`,
+    errorMessage: () => `The field is required.`,
   },
   isImage: {
     validator: (fieldName) =>
@@ -124,12 +123,12 @@ export const validationConfig = {
         const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
         if (req.file) {
           if (!allowedMimeTypes.includes(req.file.mimetype)) {
-            throw new Error(`El archivo en el campo ${fieldName} debe ser una imagen (JPEG o PNG).`);
+            throw new Error(`The file must be an image (JPEG or PNG).`);
           }
         }
         return true;
       }),
-    errorMessage: (fieldName) => `El archivo en el campo ${fieldName} debe ser una imagen (JPEG o PNG).`,
+    errorMessage: () => `The file must be an image (JPEG or PNG).`,
   },
 
   password: {
@@ -146,21 +145,20 @@ export const validationConfig = {
 
         if (!(hasUppercase && hasLowercase && hasNumber && hasSpecialChar)) {
           throw new Error(
-            `El campo ${fieldName} debe contener al menos 1 mayúscula, 1 minúscula, 1 número y 1 caracter especial`,
+            `The field must contain at least 1 uppercase, 1 lowercase, 1 number, and 1 special character`,
           );
         }
 
         return true;
       }),
-    errorMessage: (fieldName) =>
-      `El campo ${fieldName} debe contener al menos 1 mayúscula, 1 minúscula, 1 número y 1 caracter especial`,
+    errorMessage: () => `The field must contain at least 1 uppercase, 1 lowercase, 1 number, and 1 special character`,
   },
 
   OnlyLetters: {
     validator: (fieldName) =>
       body(fieldName).custom((value) => {
         // Define la expresión regular para validar nombres que solo contienen letras y espacios
-        const regex = /^[A-Za-zÁáÉéÍíÓóÚúÜüÑñ\s-.]+$/;
+        const regex = /^[A-Za-zÁáÉéÍíÓóÚúÜüÑñ\s.]+$/;
 
         if (!regex.test(value)) {
           throw new Error(`Invalid characters. Use only letters, spaces, or hyphens.`);
